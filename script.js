83be223022d73473b90ae1aea500bef10d2fe1e1
EdielD7 <129358065+EdielD7@users.noreply.github.com>
2023-06-23 14:14:29 -0400

    Adicionando valores a tabela
    Aula 4, parte 2 de 2. JS atualizado para adicionar valores do input na tabela html. Valores de referência em tbody mantidos como comentário para estilização css, caso necessário.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const form = document.getElementById('form-atividade');
+const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji festejando">'
+const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji triste">'
+
+let linhas = ''
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -6,5 +10,14 @@ form.addEventListener('submit', function(e) {
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
 
-    alert(`Atividade: ${inputNomeAtividade.value} - Nota: ${inputNotaAtividade.value}`)
+    let linha = '<tr>'
+    linha += `<td>${inputNomeAtividade.value}</td>`
+    linha += `<td>${inputNotaAtividade.value}</td>`
+    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`
+    linha += '</tr>'
+    
+    linhas += linha
+
+    const corpoTabela = document.querySelector('tbody')
+    corpoTabela.innerHTML = linhas
 })
